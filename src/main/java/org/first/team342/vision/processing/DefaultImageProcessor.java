64c4590720599c1449c4d8de50aec1b815aeac45
Adam Brightwell <abrightwell@lce.com>
2014-02-06 10:42:12 -0500

    added vision processing package.

---
+++ src/main/java/org/first/team342/vision/processing/DefaultImageProcessor.java
@@ -0,0 +1,14 @@
+package org.first.team342.vision.processing;
+
+import org.first.team342.vision.Image;
+
+/**
+ *
+ * @author FIRST Team 342
+ */
+public class DefaultImageProcessor implements ImageProcessor {
+
+    public Image processImage(Image image) {
+        return image;
+    }
+}
---
+++ src/main/java/org/first/team342/vision/processing/ImageProcessor.java
@@ -0,0 +1,11 @@
+package org.first.team342.vision.processing;
+
+import org.first.team342.vision.Image;
+
+/**
+ *
+ * @author FIRST Team 342
+ */
+public interface ImageProcessor {
+    public Image processImage(Image image);
+}
