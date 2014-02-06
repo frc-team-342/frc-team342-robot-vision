7a5e0c1b6227bf7493738f7b44e7908f9f5a8384
Adam Brightwell <abrightwell@lce.com>
2014-02-06 14:37:00 -0500

    added initial implementation for usb camera.

--- src/main/java/org/first/team342/vision/UsbCamera.java
+++ src/main/java/org/first/team342/vision/UsbCamera.java
@@ -1,32 +1,119 @@
 package org.first.team342.vision;
 
+import com.googlecode.javacv.FrameGrabber;
+import com.googlecode.javacv.OpenCVFrameGrabber;
+import com.googlecode.javacv.cpp.opencv_core;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author FIRST Team 342
  */
 public class UsbCamera implements Camera {
 
+    /**
+     * Usb Camera logger.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(UsbCamera.class);
+    /**
+     * Constant for the default capture device number.
+     */
+    public static final int DEFAULT_DEVICE = 0;
+    /**
+     * The frame grabber utilized to access the camera's image.
+     */
+    private FrameGrabber frameGrabber;
+    /**
+     * The device number of the capture device.
+     */
+    private int deviceNumber;
+
+    /**
+     * Create a new instance using the default capture device.
+     */
+    public UsbCamera() {
+        this(DEFAULT_DEVICE);
+    }
+
+    /**
+     * Create a new instance using the capture device number specified.
+     *
+     * @param deviceNumber the capture device number.
+     */
+    public UsbCamera(int deviceNumber) {
+        this.deviceNumber = deviceNumber;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void connect() {
+        logger.debug("Connecting to Usb Camera");
+        this.frameGrabber = new OpenCVFrameGrabber(this.deviceNumber);
+        this.frameGrabber.setFrameRate(DEFAULT_FRAME_RATE);
     }
-    
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public synchronized Image getImage() {
-        return null;
+        logger.debug("Getting image from Usb Camera.");
+        Image image = null;
+
+        try {
+            opencv_core.IplImage newImage = this.frameGrabber.grab();
+            image = ImageFactory.createImage(newImage);
+            newImage.release();
+        } catch (FrameGrabber.Exception ex) {
+            logger.error("Error occured getting image.", ex);
+        }
+
+        return image;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void disconnect() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        logger.debug("Disconnecting Camera.");
+
+        try {
+            this.frameGrabber.release();
+            this.frameGrabber = null;
+        } catch (FrameGrabber.Exception ex) {
+            logger.error("Error disconnecting camera.", ex);
+        }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void start() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        logger.debug("Starting Usb Camera.");
+
+        try {
+            this.frameGrabber.start();
+        } catch (FrameGrabber.Exception ex) {
+            logger.error("Error occured starting the camera.", ex);
+        }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void stop() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        logger.debug("Stopping Usb Camera.");
+
+        try {
+            this.frameGrabber.stop();
+        } catch (FrameGrabber.Exception ex) {
+            logger.error("Error occured stopping the camera.", ex);
+        }
     }
 }
